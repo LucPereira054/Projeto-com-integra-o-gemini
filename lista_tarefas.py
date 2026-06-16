cb00cbd5e97b9b925cf892ab623efeda6bd92e69
LucPereira054 <lrp06286@gmail.com>
2026-06-16 20:01:22 +0000

    Adcionando a função de integração com API do Groq e correção de modelo

--- lista_tarefas.py
+++ lista_tarefas.py
@@ -1,3 +1,15 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+client = OpenAI(
+    base_url="https://api.groq.com/openai/v1/",
+    api_key=os.environ.get("GROQ_API_KEY")
+    )
+
+
+
 tarefas=[]
 
 def adcionar_tarefas(descricao):
@@ -26,3 +38,19 @@ adcionar_tarefas("Fazer exercícios de programação")
 listar_tarefas()
 marcar_concluida(1)
 listar_tarefas()
+
+
+
+def otimizar_com_ai(lista):
+    lista_formatada="\n".join(f"-{t['descricao']} ({t['status']})" for t in lista)
+    prompt=f"analise minhas taarefas e sugira uma ordem de execução mais eficiente:\n{lista_formatada}"
+    response=client.chat.completions.create(
+    model="llama-3.3-70b-versatile",
+    messages=[{"role":"user","content":prompt}]
+        )
+    return response.choices[0].message.content
+
+
+print("\n---Otimização com AI---")
+print(otimizar_com_ai(tarefas))
+
